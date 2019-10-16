3a1a367a0f7ca152010b75f87cf3f3ae4b12ed06
George Stephanoff <george.stephanoff7@my.stjohns.edu>
2019-10-16 14:35:32 -0400

    Uploading the page layout

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, current_app
 
-def create_app():
 
-    app = Flask(__name__)
+def create_app():
+    app = Flask(__name__, template_folder="templates")
 
     #
     from app.main import bp as main_routes_bp
