43238fd73f70d03b53725efb7fdbeab48e9afd06
Je Hyun Kim <jhynkm@gmail.com>
2019-10-17 17:52:22 -0400

    Minor fixes, add blank events page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,22 @@
 from app.main import bp
 from flask import Flask, render_template, request
 
-from app.models import db, Users
+from app.models import *
 from flask_sqlalchemy import SQLAlchemy
+
+@bp.route('/testroute', methods=['GET','POST'])
+def testroute():
+    users = db.session.query(Users).all()
+    return render_template("test.html", users=users)
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    #users = Users.query.all()
-    users = db.session.query(Users).all()
-    return render_template("calender.html", users=users)
+    return render_template("calender.html")
+
+@bp.route('/events_page', methods=['GET', 'POST'])
+def events_page():
+    events = db.session.query(Event).all()
+    return render_template('events_page.html')
 
 @bp.route('/pref', methods=['GET', 'POST'])
 def index2():
