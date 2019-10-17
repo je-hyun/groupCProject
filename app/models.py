894eabeecde4879b362588699b3b68b54f044b00
Je Hyun Kim <jhynkm@gmail.com>
2019-10-16 22:56:24 -0400

    Hook models to app, fix structure

--- app/models.py
+++ app/models.py
@@ -1,50 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+#from app import db
 db = SQLAlchemy()
-'''
-    Reference database m:n (TODO: delete after)
-    class RegisteredStudent(db.Model):
-    __tablename__ = 'registeredstudent'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    grade = db.Column(db.Integer)
-    courses = db.relationship('Course', secondary='link')
-    class Course(db.Model):
-    __tablename__ = 'course'
-    id = db.Column(db.Integer, primary_key=True)
-    course_number = db.Column(db.String)
-    course_title = db.Column(db.String)
-    students = db.relationship('RegisteredStudent', secondary='link')
-    class Link(db.Model):
-    __tablename__ = 'link'
-    registeredstudent_id = db.Column(
-    db.Integer,
-    db.ForeignKey('registeredstudent.id'),
-    primary_key=True)
-    course_id = db.Column(
-    db.Integer,
-    db.ForeignKey('course.id'),
-    primary_key=True)
-    '''
-
-'''
-    class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    categories = db.relationship('Event', secondary='userSchedule')
-    username = db.Column(db.String)
-    schedule = db.relationship('Event', secondary='schedule')
-    class UserSchedule(db.Model):
-    __tablename__ = 'userSchedule'
-    event_id = db.Column(
-    db.Integer,
-    db.ForeignKey('event.id'),
-    primary_key=True
-    )
-    user_id = db.Column(
-    db.Integer,
-    db.ForeignKey('user.id'),
-    primary_key=True
-    )
-    '''
 
 class Event(db.Model):
     __tablename__ = 'event'
@@ -57,11 +13,10 @@ class Event(db.Model):
     categories = db.relationship('Category', secondary='eventCategory')
     attending_users = db.relationship('Users', secondary='Attend_Event')
     def conflictsWithEvent(event):
-        return start<=event.end && event.start<=end
+        return start<=event.end and event.start<=end
 
 class EventCategory(db.Model):
     __tablename__ = 'eventCategory'
-
     event_id = db.Column(
      db.Integer,
      db.ForeignKey('event.id'),
@@ -81,20 +36,20 @@ class Category(db.Model):
 
 class Users(db.Model):
     __tablename__ = 'users'
-    users_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String)
     lastname = db.Column(db.String)
     events = db.relationship('Event', secondary='Attend_Event')
 
 class Attend_Event(db.Model):
     __tablename__ = 'Attend_Event'
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id') primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), primary_key=True)
-    
+
 class Preference(db.Model):
     __tablename__ = 'preference'
     preference_id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Double)
+    price = db.Column(db.Float)
     location = db.Column(db.String)
     size = db.Column(db.String)
     DayFree = db.relationship(db.String)
