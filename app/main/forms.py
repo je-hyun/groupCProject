246aab189e2dffe7f1f65d6644b8a8cc3cb87070
Andrew <andrew.oleary13@stjohns.edu>
2019-10-31 13:12:54 -0400

    Updated forms, routes, and templates.

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, IntegerField
+from wtforms.validators import DataRequired
+
+class EventForm(FlaskForm):
+    name = StringField('Event Name', validators=[DataRequired()])
+    start = DateTimeField('Start Date/Time', validators=[DataRequired()])
+    end = DateTimeField('End Date/Time', validators=[DataRequired()])
+    price = IntegerField('Price')
+    location = StringField('Location')
+    submit = SubmitField('Add Event')
+
+class EventsPageForm(FlaskForm):
+    submit = SubmitField('Attend Event')
+
+#class PreferenceForm(FlaskForm):
+    
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,6 @@
 from app.main import bp
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from app.main.forms import EventForm, EventsPageForm
 
 from app.models import *
 from flask_sqlalchemy import SQLAlchemy
@@ -11,18 +12,19 @@ def testroute():
     users = db.session.query(User).all()
     my_user = db.session.query(User).get(0)
 
-    event = Event(
+    """ event = Event(
         id = 0,
         start = datetime.datetime(2018, 8, 1),
         end = datetime.datetime(2018, 8, 2),
         name = "Rock climbing event",
         price = "10",
         location = "NYC"
-    )
-    succesfullyAdded = db.session.query(User).get(0).attend_event(event)
-    succesfullyAdded = db.session.query(User).get(1).attend_event(event)
-    succesfullyAdded = db.session.query(User).get(2).attend_event(event)
-    print(succesfullyAdded)
+    ) """
+
+    #succesfullyAdded = db.session.query(User).get(0).attend_event(event)
+    #succesfullyAdded = db.session.query(User).get(1).attend_event(event)
+    #succesfullyAdded = db.session.query(User).get(2).attend_event(event)
+    #print(succesfullyAdded)
     return render_template("test.html", users=users)
 
 
@@ -32,8 +34,23 @@ def index():
 
 @bp.route('/events_page', methods=['GET', 'POST'])
 def events_page():
+    form = EventsPageForm()
+    if form.validate_on_submit():
+        event_id = request.form['event.id']
+        attend = AttendEvent(user_id=0, event_id=event_id)
+        flash('Test')
     events = Event.query.all()
-    return render_template('events_page.html', events=events)
+    return render_template('events_page.html', events=events, form=form)
+
+@bp.route('/add_events', methods=['GET', 'POST'])
+def add_events():
+    event_form = EventForm()
+    if event_form.validate_on_submit():
+        event = Event(start=event_form.start.data, end=event_form.end.data, name=event_form.name.data, price=event_form.price.data, location=event_form.location.data)
+        db.session.add(event)
+        db.session.commit()
+        flash('Event Added.')
+    return render_template('add_events.html', event_form=event_form)
 
 @bp.route('/pref', methods=['GET', 'POST'])
 def index2():
