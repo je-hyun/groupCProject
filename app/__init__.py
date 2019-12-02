7e6f69d9e1d700e55b7957ffd40e5c60d419a3e0
“[firstname <peza361@gmail.com>
2019-12-02 17:28:08 -0500

    merged master into my branch

--- app/__init__.py
+++ app/__init__.py
@@ -2,28 +2,23 @@ from flask import Flask, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
     app.config.from_object(Config)
-    db.init_app(app)
-    migrate.init_app(app, db)
-    #from app.models import db
-
-
+    #db = SQLAlchemy()
+    migrate = Migrate(app, db)
     # Build the database every time "flask run" is executed.
-
-
-    '''
     with app.test_request_context():
+        db.init_app(app)
+        login_manager.init_app(app)
         db.create_all()
-    '''
 
     from app.main import bp as main_routes_bp
     app.register_blueprint(main_routes_bp)
     return app
-
-from app import models
--- app/models.py
+++ app/models.py
@@ -1,12 +1,26 @@
 from flask import app
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from geopy.geocoders import Nominatim
 from app.location_utils import coordinatesToAddress, addressToCoordinates
-from app import db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bootstrap import Bootstrap
+from config import Config
+#bootstrap = Bootstrap(app)
 
-#from app import db
-#db = SQLAlchemy()
+'''
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+'''
 
+#login_manager = LoginManager()
+#login_manager.login_view = 'login'
+#login_manager.init_app(app)
+
+#Manager.add_command('db', MigrateCommand)
 
 class Event(db.Model):
     __tablename__ = 'event'
@@ -16,7 +30,7 @@ class Event(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.String)
 
-    location = db.Column(db.String) # TODO: Deprecate this
+    location = db.Column(db.String)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
 
@@ -48,6 +62,7 @@ class Category(db.Model):
     name = db.Column(db.String)
     events = db.relationship('Event', secondary='eventCategory', back_populates="categories")
 
+'''
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -72,6 +87,8 @@ class User(db.Model):
             db.session.commit()
             return True
 
+'''
+
 class AttendEvent(db.Model):
     __tablename__ = 'attendEvent'
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
@@ -90,13 +107,26 @@ class Preference(db.Model):
     DayFree = db.Column(db.String)
     hoursFree = db.Column(db.String)
 
-
+#Kamil WorkingForm
 class TimeSlot(db.Model):
     __tablename__ = 'timeslot'
     timeslot_id = db.Column(db.Integer, primary_key=True)
     day = db.Column(db.String)
     start_time = db.Column(db.String)
     end_time = db.Column(db.String)
+
+#Kamil Login (databse for users)
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), index=True, unique=True)
+    email = db.Column(db.String(30), index=True, unique=True)
+    password = db.Column(db.String(60))
+    events = db.relationship('Event', secondary='attendEvent', back_populates="attending_user")
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 def get_address(self):
