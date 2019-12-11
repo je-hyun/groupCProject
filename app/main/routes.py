5da43dbb3d651565c6be2c14b54407e1f559ac1b
“[firstname <peza361@gmail.com>
2019-12-10 20:55:05 -0500

    merged master into my branch

--- app/main/routes.py
+++ app/main/routes.py
@@ -50,7 +50,7 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            # login_user(user)  # , remember=form.remember_me.data
+            login_user(user)  # , remember=form.remember_me.data
             flash('Invalid Username or Password')
             return redirect(url_for('main.login'))
 
